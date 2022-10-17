b7b3e9668cfffecf64d49198cccdcd7de286682c
Caius Durling <dev@caius.name>
2022-10-17 21:48:03 +0100

    Fix file reading, break socket reading
    File::Stat appears not to exist anymore

--- cat.cr
+++ cat.cr
@@ -14,24 +14,23 @@ inputs.each do |path|
   end
 
   begin
-
-    stat = File::Stat.new(path)
-    if stat.socket?
+    # stat = File::Stat.new(path)
+    # if stat.socket?
       # Unix socket time
 
-      sock = Socket.unix
-      sock.connect Socket::UNIXAddress.new(path)
-      IO.copy(sock, STDOUT)
-
-    else
+    #   sock = Socket.unix
+    #   sock.connect Socket::UNIXAddress.new(path)
+    #   IO.copy(sock, STDOUT)
+    #
+    # else
       # Read file from path
 
       File.open(path, "r") do |fh|
         IO.copy(fh, STDOUT)
       end
 
-    end
-  rescue Errno
+    # end
+  rescue File::NotFoundError
     puts "cat: #{path}: No such file or directory"
     exit_value = 1
   end
